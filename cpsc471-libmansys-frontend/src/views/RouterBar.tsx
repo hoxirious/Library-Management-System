c343408f4cf2fe9c248c9bdad258295fdf3081d7
hoxirious <giahy33@gmail.com>
2021-11-15 21:36:33 -0700

    added router-bar & router-button

--- cpsc471-libmansys-frontend/src/views/RouterBar.tsx
+++ cpsc471-libmansys-frontend/src/views/RouterBar.tsx
@@ -1,9 +1,18 @@
+import { routeInfoMap, RouteKey, routeMap } from "components/data"
+import { ButtonRoute } from "components/common"
 import "styles/views/RouterBar.sass"
 
 export const RouterBar = () => {
     return (
         <div className="router-bar">
-            RouterBar
+            {Object.keys(routeMap).map((route) => (
+                <ButtonRoute
+                    buttonTitle={routeInfoMap[route as RouteKey].displayName}
+                    buttonActive="button-route--active"
+                    buttonStyle="button-route"
+                    routeTo={routeInfoMap[route as RouteKey].routeKey}
+                />
+            ))}
         </div>
     )
 }
