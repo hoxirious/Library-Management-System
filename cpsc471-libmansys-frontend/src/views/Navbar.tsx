a66b36072d7e05179b0e6000723a4cd96c16537c
hoxirious <giahy33@gmail.com>
2021-11-15 19:19:10 -0700

    style navbar

--- cpsc471-libmansys-frontend/src/views/Navbar.tsx
+++ cpsc471-libmansys-frontend/src/views/Navbar.tsx
@@ -1,8 +1,19 @@
+import { ButtonAction } from "components"
 import "styles/views/Navbar.sass"
 
 export const Navbar = () => {
     return (
         <div className="navbar">
+            <div className="navbar__content">
+                <div className="navbar__content--block-left">
+                    <p className="logo">LIBMANSYS.</p>
+                    <input placeholder="Search your item" className="search"></input>
+                </div>
+                <div className="navbar__content--block-right">
+                    <ButtonAction>My Cart</ButtonAction>
+                    <ButtonAction>Profile</ButtonAction>
+                </div>
+            </div>
         </div>
     )
 }
