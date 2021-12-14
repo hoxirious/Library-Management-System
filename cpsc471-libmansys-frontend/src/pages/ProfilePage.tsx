15c3697e7e0167b75f3dc1677ea075699a6e363b
hoxirious <giahy33@gmail.com>
2021-12-13 21:44:58 -0700

    mock template

---
+++ cpsc471-libmansys-frontend/src/pages/ProfilePage.tsx
@@ -0,0 +1,24 @@
+import { CustomCard } from "components/common"
+import "styles/pages/ProfilePage.sass"
+
+export const ProfilePage = () => {
+    return (
+        <div className="profile-page">
+            <CustomCard
+                header={<p>Profile</p>}
+                children={
+                    <div>
+                        <p> Name: Luca Lacu</p>
+                        <p> UCID: 300864624</p>
+                        <p> Username: luca.lacu@ucalgary.ca</p>
+                        <p> Password: ********</p>
+                        <p> Your fine: $5.02</p>
+                        <button> Change password</button>
+                    </div>
+                }
+                footer={<button>Save</button>}
+                cardStyle="profile-card"
+            />
+        </div>
+    )
+}
